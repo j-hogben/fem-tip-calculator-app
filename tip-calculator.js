91d95a6b95ecca096d4205698d2392c790dc7495
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-09-04 13:24:06 +0100

    Javascript - All styling and usability logic - NO calculator functions yet

--- tip-calculator.js
+++ tip-calculator.js
@@ -1,15 +1,66 @@
-const formElement = document.getElementById('tipCalculatorForm');
-const textInputs = formElement.querySelectorAll('input[type="text"]');
+const calculatorForm = document.getElementById('tipCalculatorForm');
+
+// /////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ CALCULATOR LOGIC ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+// /////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ CUSTOM INPUT RULES ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+const customRadio = document.getElementById('customTipRadio');
+const customAmount = document.getElementById('customTipAmount');
+const otherRadios = document.querySelectorAll('.static-amount-value');
+
+// FOCUS ON CUSTOM AMOUNT WHEN CUSTOM RADIO IS CHECKED
+customRadio.addEventListener('change', () => {
+  customAmount.focus();
+});
+
+// RESET CUSTOM AMOUNT IF OTHER RADIO BUTTON AMOUNT IS CHECKED
+otherRadios.forEach((button) => {
+  button.addEventListener('change', () => {
+    customAmount.value = '';
+  });
+});
+
+// IF CUSTOM AMOUNT IS EMPTY WHEN NAVIGATED AWAY FROM, HIDE TEXT INPUT
+customAmount.addEventListener('blur', () => {
+  if (customAmount.value === '') {
+    customRadio.checked = false;
+  }
+});
+
+// /////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ RESET BUTTON ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+const resetButton = document.getElementById('reset-btn');
+
+// RESET FORM FUNCTION
+const resetForm = () => calculatorForm.reset();
 
 // /////////////////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////////////
 
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ FORM VALIDATION ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
+// CUSTOM VALIDATION FUNCTION
 const validateForm = (formSelector) => {
-  formElement.addEventListener('submit', (event) => {
-    event.preventDefault();
-    validateAllFormGroups();
+  const formGroups = document.querySelectorAll('.form-group');
+
+  formGroups.forEach((formGroup) => {
+    const formGroupInput = formGroup.querySelector('input');
+
+    // FOR INPUT OR BLUR EVENTS ON AN INPUT FIELD, VALIDATE THAT INPUT FIELD
+    ['input', 'blur'].forEach((eventType) => {
+      formGroupInput.addEventListener(eventType, () => {
+        validateSingleFormGroup(formGroup);
+      });
+    });
   });
 
   // VALIDATION OPTIONS
@@ -29,19 +80,12 @@ const validateForm = (formSelector) => {
     for (const option of validationOptions) {
       if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
         errorContainer.textContent = option.errorMessage;
+        input.parentElement.classList.add('error-active');
       } else {
         errorContainer.textContent = '';
+        input.parentElement.classList.remove('error-active');
       }
     }
-  };
-
-  // VALIDATE ALL THE FORM GROUPS
-  const validateAllFormGroups = (formToValidate) => {
-    const formGroups = document.querySelectorAll('.form-group');
-
-    formGroups.forEach((formGroup) => {
-      validateSingleFormGroup(formGroup);
-    });
   };
 };
 
@@ -49,6 +93,7 @@ const validateForm = (formSelector) => {
 // /////////////////////////////////////////////////////////////////////////////////////////
 
 // DISABLE HTML VALIDATION
-formElement.setAttribute('novalidate', '');
+calculatorForm.setAttribute('novalidate', '');
 
-validateForm(formElement);
+// RUN CUSTOM VALIDATION FUNCTION
+validateForm(calculatorForm);
